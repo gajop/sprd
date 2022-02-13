20b089221beb9180770a1a70f0eeaff2fbb86153
Gajo Petrovic <gajopetrovic@gmail.com>
2022-02-14 01:24:46 +0900

    don't try to download if all files are there

--- src/cmds/download.rs
+++ src/cmds/download.rs
@@ -1,9 +1,17 @@
-use crate::{api::DownloadOptions, file_download, metadata_query, rapid::rapid_store::RapidStore};
+use crate::{
+    api::{check_if_sdp_needs_download, DownloadOptions},
+    file_download, metadata_query,
+    rapid::rapid_store::RapidStore,
+};
 
 pub async fn download<'a>(rapid_store: &RapidStore, opts: &DownloadOptions, fullname: &str) {
     let (repo, sdp) = metadata_query::query_metadata(rapid_store, opts, fullname).await;
 
     let sdp_files = metadata_query::query_sdp_files(rapid_store, &repo, &sdp).await;
+
+    if !check_if_sdp_needs_download(rapid_store, &sdp.md5) {
+        return;
+    }
 
     let download_map = rapid_store.get_missing_files_indices(&sdp_files);
     file_download::download_sdp_files(rapid_store, &repo, &sdp, download_map, &sdp_files)
